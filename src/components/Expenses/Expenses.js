ecf943d1b270de25ba96ec61ac294d300fbb76ea
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2023-10-22 02:07:19 +0530

    updates

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import ExpenseItem from './ExpenseItem.js';
-import Card from './Card.js';
+import Card from '../UI/Card.js';
 import './Expenses.css';
-function Expenses(props){
+const Expenses=(props)=>{
     return(
         <Card className="expenses">
         <ExpenseItem
